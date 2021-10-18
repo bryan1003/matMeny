dce8b43b90c4c39fc53ea969900b1f78789152fa
bryan1003 <bryan031@live.no>
2021-10-18 11:55:35 +0200

    Addet filer

---
+++ matMeny.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <direct.h>
+
+int main()
+{
+    int i = 1;
+    while ( i < 2);
+    {
+        printf("Velkommen til Kuben Fast Food!\n");
+        sleep(2);
+        printf("Her er menyen for idag!\n");
+        sleep(1);
+        printf("Pizza: (120kr) trykk 1 %40s Kebab: (90kr) trykk 2");
+        return 0;
+    }
+
+
+
+}
