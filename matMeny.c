6130ad75e89560ed91102894715b2745eb88aca7
bryan1003 <bryan031@live.no>
2021-11-10 09:06:09 +0100

    Addet nye ting

--- matMeny.c
+++ matMeny.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <direct.h>
+#include <time.h>
 
 int main()
 {
@@ -8,10 +8,21 @@ int main()
     while ( i < 2);
     {
         printf("Velkommen til Kuben Fast Food!\n");
-        sleep(2);
+        //sleep(2);
         printf("Her er menyen for idag!\n");
-        sleep(1);
-        printf("Pizza: (120kr) trykk 1 %40s Kebab: (90kr) trykk 2");
+        //sleep(1);
+        printf("Pizza: (120kr) trykk 1 %40s Kebab: (90kr) trykk 2\n");
+        //sleep(1);
+        int mat; 
+        scanf("%d", &mat);
+            if (mat == 1)
+            {
+                printf("Du valgte pizza 1\n");
+
+
+            }
+                
+
         return 0;
     }
 
